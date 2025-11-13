5ba3d15b720c684df9c18f1ea7097d07351030ec
Jaruny Cárdenas <160541923+xJarucL@users.noreply.github.com>
2025-11-12 22:27:53 -0700

    Refactor app.js to improve jQuery setup
    Removed console logs and streamlined jQuery initialization.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,21 +2,13 @@ import './bootstrap';
 import $ from 'jquery';
 import Alpine from 'alpinejs';
 
-// Hacer disponibles globalmente
 window.$ = $;
 window.jQuery = $;
 window.Alpine = Alpine;
 
 Alpine.start();
 
-console.log('jQuery disponible:', typeof $);
-console.log('app.js cargado correctamente');
-
-// Esperar a que el DOM y jQuery estén listos
 $(document).ready(function () {
-    console.log('DOM listo, jQuery activo');
-
-    // Cargar scripts dependientes una vez que jQuery está disponible
     import('./sweetalert.js').then(() => console.log('sweetalert.js cargado'));
     import('./funciones.js').then(() => console.log('funciones.js cargado'));
 });
