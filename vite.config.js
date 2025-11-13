d4c05ee01b0685903ddae376e37e7b35f726cbd6
Jaruny Cárdenas <160541923+xJarucL@users.noreply.github.com>
2025-11-12 20:56:40 -0700

    Add Tailwind CSS plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
