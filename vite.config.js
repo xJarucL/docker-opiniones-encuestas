082a84c048a5f3e75ab21c4c95feba1b40b64621
Jaruny Cárdenas <160541923+xJarucL@users.noreply.github.com>
2025-11-12 20:35:36 -0700

    Add new JS files to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            buildDirectory: 'build',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/funciones.js',
+                'resources/js/sweetalert.js',
+            ],
+            buildDirectory: '../build',
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
