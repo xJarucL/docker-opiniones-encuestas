47d809bad1d4e68974f6f615e085917a66aa03b6
Jaruny Cárdenas <160541923+xJarucL@users.noreply.github.com>
2025-11-12 20:52:20 -0700

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./storage/framework/views/*.php",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
